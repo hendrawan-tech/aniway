e5de20cc5d4f75b982756e0937ce5c7040aa375a
Miuuwu <zaidaliyhijja@gmail.com>
2022-02-06 18:50:36 +0700

    change domain

--- components/Fetch/fetchAnime.jsx
+++ components/Fetch/fetchAnime.jsx
@@ -1,6 +1,6 @@
 import { animeFetcher } from "aniwayscrap-api";
 const IAnimeConfig = {
-  baseUrl: "https://www4.gogoanime.cm",
+  baseUrl: "https://www1.gogoanime.pe",
   apiBaseUrl: "https://ajax.gogo-load.com",
 };
 export const fetchAnime = new animeFetcher(IAnimeConfig);
