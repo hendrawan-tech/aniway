0909dc3c0d9d2b28775231d60102c2efa1cf4b4e
Miukyo <zaidaliyhijja@gmail.com>
2022-05-13 17:48:21 +0700

    update url

--- components/Fetch/fetchAnime.jsx
+++ components/Fetch/fetchAnime.jsx
@@ -1,6 +1,6 @@
 import { animeFetcher } from "aniwayscrap-api";
 const IAnimeConfig = {
-  baseUrl: "https://gogoanime.fi/",
+  baseUrl: "https://ww2.gogoanimes.org",
   apiBaseUrl: "https://ajax.gogo-load.com",
 };
 export const fetchAnime = new animeFetcher(IAnimeConfig);
